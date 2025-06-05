b7d19c9e3148e5344304953944d769d30bb487f8
imasadali <imasadali7@gmail.com>
2025-06-05 20:39:53 +0500

    reverting changes

--- src/flagship/flagship.service.ts
+++ src/flagship/flagship.service.ts
@@ -92,7 +92,7 @@ export class FlagshipService {
         if (flagship.images && flagship.images.length > 0) {
           const imageUrls = await Promise.all(
             flagship.images.map(async (imageKey) => {
-              return await this.storageService.getImageData(imageKey);
+              return await this.storageService.getSignedUrl(imageKey);
             }),
           );
           flagshipObj.images = imageUrls;
