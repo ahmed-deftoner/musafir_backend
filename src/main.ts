fbe11c2f0e3d64a03cb681e3d456233444871362
Ahmed Nadeem <ahmedghtwhts786@gmail.com>
2025-07-07 02:15:51 +0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -45,6 +45,7 @@ async function bootstrap() {
       const allowedOrigins = [
         'https://main.d1gcdykopg01ak.amplifyapp.com',
         'http://localhost:3000',
+        'https://test.3musafir.com',
       ];
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
